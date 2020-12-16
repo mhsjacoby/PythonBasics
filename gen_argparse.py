19c0f9b466051ba535af8c807d5495c2d88cf587
mhsjacoby <margarite.jacoby@colorado.edu>
2020-12-16 15:23:41 -0800

    split cheat sheets into different files

--- gen_argparse.py
+++ gen_argparse.py
@@ -1,11 +1,13 @@
 """
 gen_argparse.py
+Update: 2020-10-19 include pi_audio for Process_count_audio.py
 general argument parsers that are used in a variety of detection, inference and file reading codes. 
 usage: python3 arg_test.py -path /Volumes/TOSHIBA-21/H1-red/ -hub RS1 RS2
 
 employed in:
 	- copy_img.py
 	- copy_audio.py
+	- Process_count_audio.py
 
 not yet in:
 	- detect.py and confidence.py (image detection using yolov5)
@@ -23,12 +25,19 @@ parser = argparse.ArgumentParser(description="Description")
 
 parser.add_argument('-path','--path', default='', type=str, help='path of stored data') # Stop at house level, example G:\H6-black\
 parser.add_argument('-save_location', '--save', default='', type=str, help='location to store files (if different from path')
-parser.add_argument('-hub', '--hub', default="", nargs="+", type=str, help='if only one hub... ') # Example: python time_window.py -hub BS2 BS3
+parser.add_argument('-hub', '--hub', default='', nargs='+', type=str, help='if only one hub... ') # Example: python time_window.py -hub BS2 BS3
 parser.add_argument('-start_date', '--start', default='', type=str, help='type day to start')
 parser.add_argument('-end_date', '--end', default='', type=str, help='type day to end')
 
 # used only for image copying
 parser.add_argument('-img_name', '--img_name', default='img-unpickled', type=str, help='name of files containing raw images')
+
+# used only for audio processing
+parser.add_argument('-pi_audio', '--pi_audio', default='yes', type=str, help='does pi audio exist? default is yes, else type no')
+
+# used only for env processing
+parser.add_argument('-pi_env', '--pi_env', default='yes', type=str, help='does pi env exist? default is yes, else type no')
+
 
 args = parser.parse_args()
 path = args.path
@@ -44,8 +53,12 @@ hubs = args.hub if len(args.hub) > 0 else sorted(mylistdir(path, bit=f'{color}S'
 start_date = args.start
 end_date = args.end
 
-
 img_name = args.img_name
+
+pi_audio = True if args.pi_audio == 'yes' else False
+pi_env = True if args.pi_env == 'yes' else False
+print(f'pi audio: {pi_audio}, pi env: {pi_env}')
+
 
 __all__ = [
 	"path",
@@ -57,5 +70,7 @@ __all__ = [
 	"hubs",
 	"start_date",
 	"end_date",
-	"img_name"
+	"img_name",
+	"pi_audio",
+	"pi_env"
 ]
